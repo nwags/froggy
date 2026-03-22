2c32d8ca2000908506283bf52a8ae3030626a165
nwags <5043095+nwags@users.noreply.github.com>
2026-03-21 23:22:06 -0400

    Squashed history after initial commit on ducky

---
+++ src/android/com/opentrons/otbtalpha/cordova/OTBTServiceAlpha.java
@@ -0,0 +1,16 @@
+package com.opentrons.otbtalpha.cordova;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
+
+public class OTBTAlpha extends BackgroundService{
+
+
+}
